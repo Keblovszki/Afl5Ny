582fcae7b81577c0dadbc2918b2aa16546e95506
Fie Hebsgaard <fiehebsgaard@gmail.com>
2012-12-13 15:30:45 +0100

    klar til at lave observer og tools

---
+++ src/hotciv/view/GameDrawing.java
@@ -0,0 +1,27 @@
+	package hotciv.view;
+
+import hotciv.framework.GameObserver;
+import hotciv.framework.Player;
+import hotciv.framework.Position;
+
+public class GameDrawing implements GameObserver {
+
+	@Override
+	public void worldChangedAt(Position pos) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void turnEnds(Player nextPlayer, int age) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void tileFocusChangedAt(Position position) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
